ff730ccb1fdf99cb2c5319f9f4ddacf453bedb00
Dmitri Zamysloff <d.zamysloff@dzcs.com>
2019-07-04 02:12:58 +0200

    Added dummy profile

---
+++ dogs/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into dogs(uuid, name, rasse, besitzer) values('26186994384109568', 'Remo', 'Labrador', 'Wolfram');
+insert into dogs(uuid, name, rasse, besitzer) values('26186994384109570', 'Nelly', 'Schäferhund', 'Michaela');
+insert into dogs(uuid, name, rasse, besitzer) values('26186994384109571', 'Ghost', 'Schattenwolf', 'Rob');
+insert into dogs(uuid, name, rasse, besitzer) values('26186994384109575', 'Paula', 'Mops', 'Michaela');
---
+++ dogs/src/main/resources/schema.sql
@@ -0,0 +1,7 @@
+create table dogs (
+    uuid varchar(36) not null,
+    name varchar(64) not null,
+    rasse varchar(64) not null,
+    besitzer varchar(64) not null,
+    primary key(uuid)
+);
